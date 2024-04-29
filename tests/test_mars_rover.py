1ef57b25644d9d36b6937888b0ac46f66901311f
dimanu-py <diegomtz126@gmail.com>
2024-04-29 16:24:49 +0200

    refactor(mars-rover): unify all tests into a single one using a list as a data provider

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -4,7 +4,21 @@ from simple_mars_rover.mars_rover import MarsRover
 
 
 class TestMarsRover:
-    
+
+    commands_providers = [
+        ("R", "0:0:E"),
+        ("RR", "0:0:S"),
+        ("RRR", "0:0:W"),
+        ("RRRR", "0:0:N"),
+        ("L", "0:0:W"),
+        ("LL", "0:0:S"),
+        ("LLL", "0:0:E"),
+        ("LLLL", "0:0:N"),
+        ("M", "0:1:N"),
+        ("MM", "0:2:N"),
+        ("MMMMMMMMM", "0:9:N")
+    ]
+
     def test_rover_is_deployed_at_initial_position(self) -> None:
         rover = MarsRover.deploy()
 
@@ -12,33 +26,8 @@ class TestMarsRover:
 
         assert position == "0:0:N"
 
-    @pytest.mark.parametrize(
-        "sequence, expected",
-        [("R", "0:0:E"), ("RR", "0:0:S"), ("RRR", "0:0:W"), ("RRRR", "0:0:N")]
-    )
-    def test_rover_can_turn_right(self, sequence: str, expected: str) -> None:
-        rover = MarsRover.deploy()
-
-        position = rover.execute(sequence)
-
-        assert position == expected
-
-    @pytest.mark.parametrize(
-        "sequence, expected",
-        [("L", "0:0:W"), ("LL", "0:0:S"), ("LLL", "0:0:E"), ("LLLL", "0:0:N")]
-    )
-    def test_rover_can_turn_left(self, sequence: str, expected: str) -> None:
-        rover = MarsRover.deploy()
-
-        position = rover.execute(sequence)
-
-        assert position == expected
-
-    @pytest.mark.parametrize(
-        "sequence, expected",
-        [("M", "0:1:N"), ("MM", "0:2:N"), ("MMMMMMMMM", "0:9:N")]
-    )
-    def test_rover_can_move_forward(self, sequence: str, expected: str) -> None:
+    @pytest.mark.parametrize("sequence, expected", commands_providers)
+    def test_rover_can_process_commands(self, sequence: str, expected: str) -> None:
         rover = MarsRover.deploy()
 
         position = rover.execute(sequence)
