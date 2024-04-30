38e7030328b5114b3c5ba7f6fb2f53f5901237e9
dimanu-py <diegomtz126@gmail.com>
2024-04-30 10:05:25 +0200

    feat(compass): implement turning behavior in compass
    Compass will be a value object and will encapsulate the behavior of turning instead of the MarsRover

--- simple_mars_rover/compass.py
+++ simple_mars_rover/compass.py
@@ -2,3 +2,23 @@ class Compass:
 
     def __init__(self, orientation: str) -> None:
         self.orientation = orientation
+
+    def turn_right(self) -> "Compass":
+        orientation = {
+            "N": "E",
+            "E": "S",
+            "S": "W",
+            "W": "N"
+        }[self.orientation]
+
+        return Compass(orientation)
+
+    def turn_left(self) -> "Compass":
+        orientation = {
+            "N": "W",
+            "W": "S",
+            "S": "E",
+            "E": "N"
+        }[self.orientation]
+
+        return Compass(orientation)
