60d2e76976550453fa9df4ac489a0db0d931a8ed
dimanu-py <diegomtz126@gmail.com>
2024-04-30 18:04:55 +0200

    feat(position): add __str__ method to represent object as a string

--- simple_mars_rover/position.py
+++ simple_mars_rover/position.py
@@ -4,3 +4,6 @@ class Position:
     def __init__(self, x_coordinate: int, y_coordinate: int) -> None:
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
+
+    def __str__(self) -> str:
+        return f"{self.x_coordinate}:{self.y_coordinate}"
