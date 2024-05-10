e1328b074d1cd2813b4e9db7f5b4d0121fe410c6
dimanu-py <diegomtz126@gmail.com>
2024-05-10 08:55:04 +0200

    feat(command): create TurnRight command

---
+++ simple_mars_rover/command.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+from simple_mars_rover.mars_rover import MarsRover
+
+
+class Command(ABC):
+
+    @abstractmethod
+    def execute(self) -> None:
+        """Execute the command"""
+
+
+class TurnRight(Command):
+    """Turn right command"""
+
+    def __init__(self, mars_rover: MarsRover) -> None:
+        self.mars_rover = mars_rover
+
+    def execute(self) -> None:
+        self.mars_rover.turn_right()
